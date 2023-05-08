ed8173c11a1225a9599c6d05decdc24351eea40a
arbit1998 <129003507+arbit1998@users.noreply.github.com>
2023-05-08 18:45:03 -0400

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const blogRoutes = require('./routes/blogRoutes');
 const app = express();
 
 // connect to mongodb & listen for requests
-const dbURI = "mongodb+srv://netninja:test1234@net-ninja-tuts-del96.mongodb.net/node-tuts";
+const dbURI = "mongodb+srv://cluster0.vf0tuxj.mongodb.net" --apiVersion 1 --username abit";
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(result => app.listen(3000))
